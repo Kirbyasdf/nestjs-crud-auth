872fcc991a8d29d0acf3075a6249a155901320e4
Kirbyasdf <kirbyAugustus@gmail.com>
2020-04-06 03:37:25 -0400

    1

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import * as uuid from 'uuid/v1';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = []; //if not made private any other component can adjust
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
